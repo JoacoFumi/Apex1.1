cb716eb66596121f9e75663e848aa70d28de189f
JoacoFumi <joaquinfumi@gmail.com>
2026-06-25 23:45:30 -0300

    Actualización 25/6
    Ultimas modificaciones a la fecha

--- js/script2.js
+++ js/script2.js
@@ -50,15 +50,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Ocultar logo al scrollear
-  const header = document.querySelector('.header');
-
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      header.classList.add('header--scrolled');
-    } else {
-      header.classList.remove('header--scrolled');
-    }
+  if (window.scrollY > 50) {
+    header.classList.add('header--scrolled');
+  } else {
+    header.classList.remove('header--scrolled');
+  }
   });
 
 });
